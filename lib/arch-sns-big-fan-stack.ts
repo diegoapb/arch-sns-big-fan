10aa4a8cde8c4473e42593c0d345152c26f960e5
diegoapb <parra.diego@correounivalle.edu.co>
2022-01-10 12:40:39 -0500

    ADD api gateway and Add permissions to SNS

--- lib/arch-sns-big-fan-stack.ts
+++ lib/arch-sns-big-fan-stack.ts
@@ -5,6 +5,8 @@ import * as sns_sub from "aws-cdk-lib/aws-sns-subscriptions";
 import * as sqs from "aws-cdk-lib/aws-sqs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import { SqsEventSource } from "aws-cdk-lib/aws-lambda-event-sources";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as apigw from "aws-cdk-lib/aws-apigateway";
 
 export class ArchSnsBigFanStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -104,5 +106,119 @@ export class ArchSnsBigFanStack extends Stack {
     sqsAnyOtherStatusSubscribeLambda.addEventSource(
       new SqsEventSource(anyOtherStatusQueue, {})
     );
+    /**
+     * API Gateway Creation
+     * This is complicated because it transforms the incoming json payload into a query string url
+     * this url is used to post the payload to sns without a lambda inbetween 
+     */
+   let gateway = new apigw.RestApi(this, 'theBigFanAPI', {
+    deployOptions: {
+      metricsEnabled: true,
+      loggingLevel: apigw.MethodLoggingLevel.INFO,
+      dataTraceEnabled: true,
+      stageName: 'prod'
+    }
+  });
+
+  //Give our gateway permissions to interact with SNS
+  let apigwSnsRole = new iam.Role(this, 'DefaultLambdaHanderRole', {
+    assumedBy: new iam.ServicePrincipal('apigateway.amazonaws.com')
+  });
+  topic.grantPublish(apigwSnsRole);
+
+  //Because this isn't a proxy integration, we need to define our response model
+  const responseModel = gateway.addModel('ResponseModel', {
+    contentType: 'application/json',
+    modelName: 'ResponseModel',
+    schema: { 'schema': apigw.JsonSchemaVersion.DRAFT4, 'title': 'pollResponse', 'type': apigw.JsonSchemaType.OBJECT, 'properties': { 'message': { 'type': apigw.JsonSchemaType.STRING } } }
+  });
+  
+  // We define the JSON Schema for the transformed error response
+  const errorResponseModel = gateway.addModel('ErrorResponseModel', {
+    contentType: 'application/json',
+    modelName: 'ErrorResponseModel',
+    schema: { 'schema': apigw.JsonSchemaVersion.DRAFT4, 'title': 'errorResponse', 'type': apigw.JsonSchemaType.OBJECT, 'properties': { 'state': { 'type': apigw.JsonSchemaType.STRING }, 'message': { 'type': apigw.JsonSchemaType.STRING } } }
+  });
+
+  //Create an endpoint '/InsertItem' which accepts a JSON payload on a POST verb
+  gateway.root.addResource('SendEvent')
+    .addMethod('POST', new apigw.Integration({
+      type: apigw.IntegrationType.AWS, //native aws integration
+      integrationHttpMethod: "POST",
+      uri: 'arn:aws:apigateway:us-east-2:sns:path//', // This is how we setup an SNS Topic publish operation.
+      options: {
+        credentialsRole: apigwSnsRole,
+        requestParameters: {
+          'integration.request.header.Content-Type': "'application/x-www-form-urlencoded'" // Tell api gw to send our payload as query params
+        },
+        requestTemplates: {
+        // This is the VTL to transform our incoming request to post to our SNS topic
+        // Check: https://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-mapping-template-reference.html
+        'application/json': "Action=Publish&"+
+                            "TargetArn=$util.urlEncode('"+topic.topicArn+"')&"+
+                            "Message=$util.urlEncode($input.path('$.message'))&"+
+                            "Version=2010-03-31&"+
+                            "MessageAttributes.entry.1.Name=status&"+
+                            "MessageAttributes.entry.1.Value.DataType=String&"+
+                            "MessageAttributes.entry.1.Value.StringValue=$util.urlEncode($input.path('$.status'))"
+      },
+      passthroughBehavior: apigw.PassthroughBehavior.NEVER,
+      integrationResponses: [
+        {
+          // Tells APIGW which response to use based on the returned code from the service
+          statusCode: "200",
+          responseTemplates: {
+            // Just respond with a generic message
+            // Check https://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-mapping-template-reference.html
+            'application/json': JSON.stringify({ message: 'message added to topic'})
+          }
+        },
+        {
+          // For errors, we check if the response contains the words BadRequest
+          selectionPattern: '^\[Error\].*',
+          statusCode: "400",
+          responseTemplates: {
+              'application/json': JSON.stringify({ state: 'error', message: "$util.escapeJavaScript($input.path('$.errorMessage'))" })
+          },
+          responseParameters: {
+              'method.response.header.Content-Type': "'application/json'",
+              'method.response.header.Access-Control-Allow-Origin': "'*'",
+              'method.response.header.Access-Control-Allow-Credentials': "'true'"
+          }
+        }
+      ]
+      }
+    }),
+    {
+      methodResponses: [ //We need to define what models are allowed on our method response
+        {
+          // Successful response from the integration
+          statusCode: '200',
+          // Define what parameters are allowed or not
+          responseParameters: {
+            'method.response.header.Content-Type': true,
+            'method.response.header.Access-Control-Allow-Origin': true,
+            'method.response.header.Access-Control-Allow-Credentials': true
+          },
+          // Validate the schema on the response
+          responseModels: {
+            'application/json': responseModel
+          }
+        },
+        {
+          // Same thing for the error responses
+          statusCode: '400',
+          responseParameters: {
+            'method.response.header.Content-Type': true,
+            'method.response.header.Access-Control-Allow-Origin': true,
+            'method.response.header.Access-Control-Allow-Credentials': true
+          },
+          responseModels: {
+            'application/json': errorResponseModel
+          }
+        }
+      ]
+    })
   }
+  
 }
